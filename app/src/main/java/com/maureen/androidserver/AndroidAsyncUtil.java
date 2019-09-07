ea18b3f01553f76557508f9a95eba7362f0f2042
Maureen <467914885@qq.com>
2019-09-07 13:59:39 +0800

    NanoHttpd commit

--- app/src/main/java/com/maureen/androidserver/AndroidAsyncUtil.java
+++ app/src/main/java/com/maureen/androidserver/AndroidAsyncUtil.java
@@ -1,11 +1,32 @@
 package com.maureen.androidserver;
 
+import com.koushikdutta.async.http.server.AsyncHttpServer;
+import com.koushikdutta.async.http.server.AsyncHttpServerRequest;
+import com.koushikdutta.async.http.server.AsyncHttpServerResponse;
+import com.koushikdutta.async.http.server.HttpServerRequestCallback;
+
 /**
  * @Description AndroidAsync工具类
  * @Author Lianml
  * @Create 2019-09-07 12:01
  */
 public class AndroidAsyncUtil {
+    private AsyncHttpServer asyncHttpServer;
+    private int port;
 
+    public AndroidAsyncUtil(int port) {
+        this.port = port;
+    }
+
+    public void startServer(){
+        asyncHttpServer = new AsyncHttpServer();
+        asyncHttpServer.listen(port);
+        asyncHttpServer.get("/", new HttpServerRequestCallback() {
+            @Override
+            public void onRequest(AsyncHttpServerRequest request, AsyncHttpServerResponse response) {
+
+            }
+        });
+    }
 
 }
--- app/src/main/java/com/maureen/androidserver/MainActivity.java
+++ app/src/main/java/com/maureen/androidserver/MainActivity.java
@@ -1,18 +1,36 @@
 package com.maureen.androidserver;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.IOException;
 
 /**
  * @Author Lianml
  * @Create 2019-09-07 11:58
  */
 public class MainActivity extends AppCompatActivity {
+    private NanohttpdServer mNanohttpdServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.activity_main );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void createNanoHttpdServer(int port, NanohttpdServer nanohttpdServer) {
+        nanohttpdServer = new NanohttpdServer(8080);
+        try {
+            nanohttpdServer.start();
+            Toast.makeText(MainActivity.this,"服务开启",Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createAndroidAsyncServer(){
+
     }
 }
